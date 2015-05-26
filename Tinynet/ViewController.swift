60cda9b82c7200b68976bc34f840e6575ca8d1dd
zhwayne <zhwayne@qq.com>
2015-05-26 18:29:30 +0800

    delete message

--- Tinynet/ViewController.swift
+++ Tinynet/ViewController.swift
@@ -13,24 +13,7 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        
-        var info:[String:AnyObject] = [:]
-        info["userId"] = "0"
-        info["key"] = "7506BAF5-1A21-4240-A9E2-3E1DD1B61D30"
-        info["device"] = "2"
-        info["content"] = "JustForTest"
-        
-        println(info)
-        
-        Tinynet.post(url: "http://1.wwszgroup.sinaapp.com/0/users/feedback/add.php", params: info) { (data, response, error) -> Void in
-            if let er = error {
-                println(er.description)
-            }
-            else {
-                let str = NSString(data: data, encoding: NSUTF8StringEncoding)!
-                println(str)
-            }
-        }
+    
     }
 
     override func didReceiveMemoryWarning() {
